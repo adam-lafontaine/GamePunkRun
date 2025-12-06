1673df75de929833b722c2da37bb6ab273da64ae
adam-lafontaine <adam.lafontiane@gmail.com>
2025-12-06 10:32:21 -0500

    move main_o files

--- game_punk/src/pltfm/ubuntu_sdl3/ubuntu_sdl3_punk_main.cpp
+++ game_punk/src/pltfm/ubuntu_sdl3/ubuntu_sdl3_punk_main.cpp
@@ -1,11 +1,11 @@
 #ifdef NDEBUG
 
-#include "main_o.cpp"
+#include "../main_o/main_o_sdl3.cpp"
 #include "../punk_main.cpp"
 
 #else
 
 #include "../punk_main.cpp"
-#include "main_o.cpp"
+#include "../main_o/main_o_sdl3.cpp"
 
 #endif
