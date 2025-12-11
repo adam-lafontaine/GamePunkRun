0a2c1b1dffa42a121661b843bbbedc1488b741d0
adam-lafontaine <adam.lafontiane@gmail.com>
2025-12-11 18:38:12 -0500

    builds in zig-out

--- game_punk/src/pltfm/zig/build.zig
+++ game_punk/src/pltfm/zig/build.zig
@@ -5,16 +5,28 @@ const main_cpp = "../ubuntu_sdl2/ubuntu_sdl2_punk_main.cpp";
 const app_name = "punk_run";
 
 
-const cpp_flags = &[_][]const u8 {
+const root_dir = "../../..";
+const build_dir = root_dir ++ "/build/ubuntu_sdl2";
+const res_dir = root_dir ++ "/res";
+const bin_data = res_dir ++ "/xbin/punk_run.bin";
+
+
+const cpp_flags = &[_][]const u8{
     "-std=c++20",
     "-mavx",
     "-mavx2",
     "-mfma",
-    "-DGAME_PUNK_RELEASE",
-    "-DAPP_ROTATE_90",
     "-DNDEBUG",
     "-O3",
+    "-DGAME_PUNK_RELEASE",
+    "-DAPP_ROTATE_90",
     "-DIMAGE_READ",
+};
+
+
+const cpp_libs = &[_][]const u8{
+    "SDL2",
+    "SDL2_mixer"
 };
 
 
@@ -39,22 +51,25 @@ pub fn build(b: *std.Build) void
         .flags = cpp_flags,
     });
 
-    exe.linkSystemLibrary("SDL2");
-    exe.linkSystemLibrary("SDL2_mixer");
+    for (cpp_libs) |lib|
+    {
+        exe.linkSystemLibrary(lib);
+    }
+    
     exe.linkLibC();
-    exe.linkLibCpp();        // Required for <iostream>, std::cout, etc.
+    exe.linkLibCpp();
 
     b.installArtifact(exe);
 
-    // zig build run
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| 
-    {
-        run_cmd.addArgs(args);
-    }
-    b.step("run", "Run the app").dependOn(&run_cmd.step);
+    // Copy punk_run.bin next to the exe in zig-out/bin
+    const copy_data = b.addInstallBinFile(
+        b.path(bin_data),
+        "punk_run.bin",
+    );
+    b.getInstallStep().dependOn(&copy_data.step);
+    
+    
 }
 
 // zig build -Doptimize=Debug          # full debug symbols, no -O3, assertions on
